b0e5af7d074b4f5ae4cd2531de3e41f02d817d4b
Wilson-Ochieng <wilsonochieng718@gmail.com>
2022-11-12 16:21:39 +0300

    Completed Assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,23 @@
 class ApplicationController < Sinatra::Base
+set :default_content_type, 'application/json'
+  get '/games' do
+  # get all the games from the database
+    games = Game.all.order(:title).limit(10)
 
-  get '/' do
-    { message: "Hello world" }.to_json
+    games.to_json
   end
-
+# use the :id syntax to create a dynamic route
+  get '/games/:id' do
+    # look up the game in the database using its ID
+    game = Game.find(params[:id])
+  
+    
+    game.to_json
+    game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } }
+    })
+    
+  end
 end
